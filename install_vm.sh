d6439101bdee8aa6549a1d11087ba182e7b581a0
safderun <burakberkkeskin@gmail.com>
2023-11-14 12:42:47 +0300

    added version and help message

--- install_vm.sh
+++ install_vm.sh
@@ -3,9 +3,35 @@ set -o pipefail
 retryAttempt=1
 retryInterval=30
 retryMaxLimit=10
+version=v1.0.0
 
 mkVmDir() {
   mkdir -p "$HOME/.tart/vms/$vmImageName"
+}
+
+printVersion() {
+  echo "${version}"
+}
+
+printHelp() {
+  printf '%s\n' "Install, validate, extract macOS vm."
+  printf '%s\n' "By default, latest macOS vm image will be installed."
+  printf 'Usage: %s [macOS-vm-name] [-h|--help]\n' "$0"
+  printf '\t%s\n' "macOS-vm-name: Specify the macOS vm name optionally."
+  printf '\t%s\n' "-h, --help: Prints help"
+}
+
+parseArguments() {
+  args=("$@")
+  for arg in "${args[@]}"; do
+    if [[ $arg == "--help" || $arg == "-h" ]]; then
+      printHelp
+      exit 0
+    elif [[ $arg == "--version" || $arg == "-v" ]]; then
+      printVersion
+      exit 0
+    fi
+  done
 }
 
 getTheLatestVmImageName() {
@@ -49,6 +75,8 @@ checkMd5Sum() {
 }
 
 main() {
+  parseArguments "$@"
+
   vmImageName=$1
   if [[ -z $vmImageName ]]; then
     vmImageName=$(getTheLatestVmImageName)
