5942153ce6f8ccd4d8293f18faa0f79f8d084738
safderun <burakberkkeskin@gmail.com>
2024-04-25 09:06:05 -0400

    made the script single file

--- download-appcircle-server.sh
+++ download-appcircle-server.sh
@@ -2,7 +2,6 @@
 set -eou pipefail
 
 credJsonPath="./cred.json"
-gloucdAuthenticatorPath="./lib/gcloudAccessToken.sh"
 gcloudAccessToken=""
 userId=""
 version="0.1.0"
@@ -53,7 +52,13 @@ extract_user_id() {
 }
 
 authenticate_gcs() {
-  gcloudAccessToken=$($gloucdAuthenticatorPath "$credJsonPath" "https://www.googleapis.com/auth/devstorage.read_only")
+  credJsonPath=$1
+  scope=$2
+  jwtToken=$(createJWTGoogleCloud "$credJsonPath" "$scope")
+  gcloudAccessToken=$(curl -s -X POST https://www.googleapis.com/oauth2/v4/token \
+    --data-urlencode 'grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer' \
+    --data-urlencode "assertion=$jwtToken" |
+    grep -oP '"access_token":"\K[^"]+')
 }
 
 download_appcircle_server_package() {
@@ -75,12 +80,47 @@ download_index_file() {
     "https://storage.googleapis.com/storage/v1/b/${bucket}/o/${objectDir}${indexFile}?alt=media")"
 }
 
+createJWTGoogleCloud() {
+  valid_for_sec="${3:-3600}"
+  #private_key=$(jq -r .private_key "$dockerCredFile")
+  json_data=$(<"$credJsonPath")
+  private_key=$(echo "$json_data" | grep -oP '"private_key": "\K(.*)(?=")')
+  sa_email=$(echo "$json_data" | grep -oP '"client_email": "\K(.*)(?=")')
+
+  header='{"alg":"RS256","typ":"JWT"}'
+  exp=$(($(date +%s) + "$valid_for_sec"))
+  iat=$(date +%s)
+
+  claim=$(
+    cat <<EOF
+{
+    "iss": "$sa_email",
+    "scope": "$scope",
+    "aud": "https://www.googleapis.com/oauth2/v4/token",
+    "exp": $exp,
+    "iat": $iat
+}
+EOF
+  )
+  request_body="$(base64var "$header").$(base64var "$claim")"
+  signature=$(echo "$private_key" | openssl dgst -sha256 -sign <(echo -e "$private_key") <(echo -n "$request_body") | base64stream)
+  printf "$request_body.$signature"
+}
+
+base64var() {
+  printf "$1" | base64stream
+}
+
+base64stream() {
+  base64 | tr '/+' '_-' | tr -d '=\n'
+}
+
 main() {
   parse_arguments "$@"
   echo "Downloading Appcircle Server zip package."
   check_cred_json
   extract_user_id
-  authenticate_gcs
+  authenticate_gcs "$credJsonPath" "https://www.googleapis.com/auth/devstorage.read_only"
   download_index_file
   download_appcircle_server_package
 }
