01ff0d78e4361b454c93bd68602ce91b5b35b121
burakberkkeskin <burakberkkeskin@gmail.com>
2024-05-14 23:18:44 +0300

    not found test added
    Signed-off-by: burakberkkeskin <burakberkkeskin@gmail.com>

--- tests/download-runner-tests.sh
+++ tests/download-runner-tests.sh
@@ -2,44 +2,61 @@
 
 source ./tests/shunit2-setup.sh
 
-testRunnerDownload(){
+testRunnerDownload() {
   echo "${testId} test starting"
-  $runnerDownloadScript test &> "$stdoutF"
+  $runnerDownloadScript test &>"$stdoutF"
 
   macOSValidMd5="bcf4afb27d8da7034577047979715934"
   assertContains "Valid remote MD5 for macOS images should be found." "$(cat "${stdoutF}")" "Valid MD5: $macOSValidMd5"
   assertContains "MD5 check for macOS image should be successfull." "$(cat "${stdoutF}")" "Your downloaded file macOS_test.tar.gz is valid"
-  
+
   xcodesValidMd5="bcf4afb27d8da7034577047979715934"
   assertContains "Valid remote MD5 for Xcode images should be found." "$(cat "${stdoutF}")" "Valid MD5: $xcodesValidMd5"
   assertContains "MD5 check for Xcodes image should be successfull." "$(cat "${stdoutF}")" "Your downloaded file xcodes_test.tar.gz is valid"
 
-
   tartVmsDirectory="$HOME/.tart/vms"
   assertContains "MacOS image should be extracted successfully." "$(cat "${stdoutF}")" "macOS_test.tar.gz is extracted to the $tartVmsDirectory/macOS_test"
   assertContains "MacOS image directory should be found in the \"$HOME/.tart/vms\" directory." "$(ls -l "$tartVmsDirectory")" "macOS_test"
-  
-  # @TODO: Will be uncommented after the macOS_test test file meets the requirements. 
+
+  # @TODO: Will be uncommented after the macOS_test test file meets the requirements.
   #assertContains "MacOS VM directory should contain \"config.json\" file." "$(ls -l "$tartVmsDirectory/macOS_test")" "config.json"
   #assertContains "MacOS VM directory should contain \"nvram.bin\" file." "$(ls -l "$tartVmsDirectory/macOS_test")" "nvram.bin"
   #assertContains "MacOS VM directory should contain \"disk.img\" file." "$(ls -l "$tartVmsDirectory/macOS_test")" "disk.img"
 
   assertContains "Xcode images should be extracted successfully." "$(cat "${stdoutF}")" "xcodes_test.tar.gz is extracted to the $HOME/images"
   assertContains "Xcode images directory should be found in the \"$HOME\" directory." "$(ls -l "$HOME")" "images"
-  
-  # @TODO: Will be uncommented after the macOS_test test file meets the requirements. 
+
+  # @TODO: Will be uncommented after the macOS_test test file meets the requirements.
   assertContains "Xcode images directory should contain some dmg files." "$(ls -l "$HOME/images")" "dmg"
-  
-  # @TODO: Will be uncommented after the macOS_test test file meets the requirements. 
+
+  # @TODO: Will be uncommented after the macOS_test test file meets the requirements.
   #assertContains "Tart list should contain the new macOS VM name" "$(tart list)" "macOS_test"
 
-  assertContains "Script output should container success log." "$(cat "${stdoutF}")" "The Appcircle runner macOS VM and Xcode images have been installed successfully"
+  assertContains "Script output should containe success log." "$(cat "${stdoutF}")" "The Appcircle runner macOS VM and Xcode images have been installed successfully"
 
 }
 
+testRunnerDownloadWithNonExistingVersion() {
+  echo "${testId} test starting"
+
+  # Change the default interval and max try values.
+  tempScript=$(mktemp)
+  newRetryInterval=1
+  newRetryMaxLimit=5
+  sed "s/retryInterval=30/retryInterval=${newRetryInterval}/; s/retryMaxLimit=10/retryMaxLimit=${newRetryMaxLimit}/" "$runnerDownloadScriptPath" >"$tempScript"
+
+  bash "$tempScript" nonExistingVersion &>"$stdoutF"
+  errorNotFoundCount=$(grep -o "The requested URL returned error: 404" "${stdoutF}" | wc -l)
+  errorNotFoundCount="${errorNotFoundCount// /}"
+  assertEquals "Script output should containe 404 logs 6 times." "6" "$errorNotFoundCount"
+
+  assertContains "Script output should containe fail log." "$(cat "${stdoutF}")" "Failed to download macOS_nonExistingVersion.tar.gz"
+
+}
 
 oneTimeSetUp() {
-  runnerDownloadScript="bash ./download-runner.sh"
+  runnerDownloadScriptPath="./download-runner.sh"
+  runnerDownloadScript="bash $runnerDownloadScriptPath"
 }
 
 setUp() {
